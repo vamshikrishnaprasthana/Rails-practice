9f99bd0579da2ce6fcd97f8deba2658cd699a617
vamshikrishnaprasthana <vamshikrishna.pandi@parasthana.com>
2022-07-13 13:02:56 +0530

    k

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_09_153001) do
+ActiveRecord::Schema.define(version: 2022_07_13_073225) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string "group_name"
